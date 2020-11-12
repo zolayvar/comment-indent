018fe106475e75639db509ab5da4de628fc60109
Stephanie Zolayvar <zolayvars@gmail.com>
2020-11-11 20:58:19 -0800

    f

--- lua/inner-comment-indent/init.lua
+++ lua/inner-comment-indent/init.lua
@@ -1,7 +1,9 @@
 local ici = {}
 
 ici.get_comment_delimiter = function()
-  return vim.api.nvim_command(":echo split(&commentstring, '%s')[0]")
+  -- return vim.api.nvim_get_option(":echo split(&commentstring, '%s')[0]")
+  print(vim.api.nvim_get_option("commentstring"))
+  return vim.api.nvim_get_option("commentstring")
 end
 
 ici.inner_indent_comment = function()
