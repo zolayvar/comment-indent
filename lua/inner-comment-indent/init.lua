b861f4fa4e07253f6de33b0a0288d482f02ac9ea
Stephanie Zolayvar <zolayvars@gmail.com>
2020-11-11 19:59:12 -0800

    g

--- lua/inner-comment-indent/init.lua
+++ lua/inner-comment-indent/init.lua
@@ -1,6 +1,14 @@
 local ici = {}
 
 ici.inner_indent_comment = function()
+  local comment_delimiter = vim.api.nvim_command([[:set commentstring?]])
+  print(comment_delimiter)
+  vim.api.nvim_command([[:'<,'> s/\/\//\/\/  / | gv]])
+end
+
+ici.inner_unindent_comment = function()
+  local comment_delimiter = vim.api.nvim_command([[:set commentstring?]])
+  print(comment_delimiter)
   vim.api.nvim_command([[:'<,'> s/\/\//\/\/  / | gv]])
 end
 
--- plugin/inner-comment-indent.vim
+++ plugin/inner-comment-indent.vim
@@ -1,2 +1,3 @@
 lua ici = require("inner-comment-indent")
-xmap <M-C-I> :lua ici.inner_indent_comment()<CR>
+xmap <Tab> :lua ici.inner_indent_comment()<CR>
+xmap <S-Tab> :lua ici.inner_unindent_comment()<CR>
